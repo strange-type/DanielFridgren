d8f70b825d7517e093e300123db72903ec9f67f4
Daniel Fridgren <4299680+dialysix@users.noreply.github.com>
2025-12-20 21:18:08 +0100

    Switch to webhook-based ConvertKit integration

--- netlify/functions/subscribe-email.js
+++ netlify/functions/subscribe-email.js
@@ -2,7 +2,7 @@ const { EMAIL_TOKEN } = process.env;
 import fetch from 'node-fetch';
 
 exports.handler = async (event, context) => {
-    const email = JSON.parse(event.body).payload.email
+    const email = JSON.parse(event.body).data.email
     console.log(`Received a submission: ${email}`)
 
     const response = await fetch(
@@ -20,9 +20,7 @@ exports.handler = async (event, context) => {
     let responseText = await response.text();
     console.log('response:', responseText);
     return {
-        statusCode: 302,
-        headers: {
-            'Location': '/confirmation',
-        },
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Success' })
     }
 }
